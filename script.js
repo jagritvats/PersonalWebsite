ef05589048d21b85ab8e30886fff0ef401dc561f
Jagrit Vats <69034224+jagritvats@users.noreply.github.com>
2022-07-16 13:15:10 +0530

    Added dark theme and changed links to open in a new tab

--- script.js
+++ script.js
@@ -1,51 +1,145 @@
+const roleElement = document.getElementById('role-text');
 
-const roleElement = document.getElementById("role-text");
-
-const roles = ["Web Developer", "Cloud Enthusiast", "Coder", "Cyber Sec Enthusiast"]
-var roleIndex = 0, textLength = roles[roleIndex].length;
+const roles = [
+	'Web Developer',
+	'Cloud Enthusiast',
+	'Coder',
+	'Cyber Sec Enthusiast',
+];
+var roleIndex = 0,
+	textLength = roles[roleIndex].length;
 
 var removeInterval, addInterval;
 
-function removeText(){
-    roleElement.innerText = roles[roleIndex].substring(0,textLength-1);
-    textLength--;
-    if(textLength==0){
-        clearInterval(removeInterval);
-        roleIndex = (roleIndex + 1) % roles.length;
-        textLength = 0;
-        addInterval = setInterval(addText, 200);
-    }
+function removeText() {
+	roleElement.innerText = roles[roleIndex].substring(0, textLength - 1);
+	textLength--;
+	if (textLength == 0) {
+		clearInterval(removeInterval);
+		roleIndex = (roleIndex + 1) % roles.length;
+		textLength = 0;
+		addInterval = setInterval(addText, 200);
+	}
 }
 
-function addText(){
-    roleElement.innerText = roles[roleIndex].substring(0, textLength+1); // adding space in innerText didn't increase it's length in HTML
-    textLength++;
-    if(textLength == roles[roleIndex].length){
-        clearInterval(addInterval);
-        textLength = roles[roleIndex].length;
-        setTimeout(()=>{
-            removeInterval = setInterval(removeText, 200);
-        },500); // pause for a while after text is completed
-        
-    }
+function addText() {
+	roleElement.innerText = roles[roleIndex].substring(0, textLength + 1); // adding space in innerText didn't increase it's length in HTML
+	textLength++;
+	if (textLength == roles[roleIndex].length) {
+		clearInterval(addInterval);
+		textLength = roles[roleIndex].length;
+		setTimeout(() => {
+			removeInterval = setInterval(removeText, 200);
+		}, 500); // pause for a while after text is completed
+	}
 }
 
-function init(){
-    roleElement.innerText = roles[roleIndex];
-    setTimeout(()=>{
-        removeInterval = setInterval(removeText, 200);
-    }, 500);
+function switchTheme() {
+	// --primary-dark-bg: #483756;
+	// --secondary-dark-bg: #564785;
+	// --tertiary-dark-bg: #343431;
+	//
+	// --primary-darktheme-fg: #FFFADE;
+	// --secondary-darktheme-fg: #F0C961;
+	// --tertiary-darktheme-fg: #F6EBFF;
+	// swap
+	// get current theme
 
-    var imgs = document.getElementsByTagName("img");
-    imgs = [...imgs];
-    imgs.forEach(img => {
-        img.ondragstart = () => {
-            return false;
-        };
-    });
+	const root = document.querySelector(':root');
 
-    // manually set skill images?
+	const props = getComputedStyle(root);
+
+	const currentTheme = props.getPropertyValue('--theme');
+
+	let primarydarkbg = props.getPropertyValue('--primary-dark-bg');
+	let secondarydarkbg = props.getPropertyValue('--secondary-dark-bg');
+	let tertiarydarkbg = props.getPropertyValue('--tertiary-dark-bg');
+	let plaincol = props.getPropertyValue('--plain-col');
+
+	let primarydarkthemefg = props.getPropertyValue('--primary-darktheme-fg');
+	let secondarydarkthemefg = props.getPropertyValue(
+		'--secondary-darktheme-fg'
+	);
+	let tertiarydarkthemefg = props.getPropertyValue('--tertiary-darktheme-fg');
+	let plaindark = props.getPropertyValue('--plain-dark');
+
+	// we don't need to even check, just swap ( later: update var names )
+	root.style.setProperty('--primary-dark-bg', primarydarkthemefg);
+	root.style.setProperty('--secondary-dark-bg', secondarydarkthemefg);
+	root.style.setProperty('--tertiary-dark-bg', tertiarydarkthemefg);
+	root.style.setProperty('--plain-col', plaindark);
+
+	root.style.setProperty('--primary-darktheme-fg', primarydarkbg);
+	root.style.setProperty('--secondary-darktheme-fg', secondarydarkbg);
+	root.style.setProperty('--tertiary-darktheme-fg', tertiarydarkbg);
+	root.style.setProperty('--plain-dark', plaincol);
+
+	// extra elems
+	const more_cert_btn = document.querySelector('.more_cert_btn');
+	const hero_section = document.querySelector('.heroSection__right img');
+	const theme_btn = document.querySelector('#themeBtn img');
+
+	//project__links
+	const plinks = document.querySelectorAll('.project__link img');
+	//findMe
+	const footer_icons = document.querySelectorAll('.footer__icon');
+	if (currentTheme == 'dark') {
+		// change the theme to light
+		root.style.setProperty('--theme', 'light');
+		root.style.setProperty('color-scheme', 'light');
+		more_cert_btn.style.setProperty('filter', 'invert(1)');
+		hero_section.style.setProperty('filter', 'invert(1)');
+		theme_btn.style.setProperty('filter', 'invert(1)');
+
+		for (let i = 0; i < plinks.length; i++) {
+			plinks[i].style.setProperty('filter', 'invert(1)');
+		}
+		for (let i = 0; i < footer_icons.length; i++) {
+			footer_icons[i].style.setProperty('filter', 'invert(1)');
+		}
+
+		localStorage.setItem('theme', 'light');
+	} else {
+		// change the theme to dark
+		root.style.setProperty('--theme', 'dark');
+		root.style.setProperty('color-scheme', 'dark');
+		more_cert_btn.style.setProperty('filter', 'none');
+		hero_section.style.setProperty('filter', 'none');
+		theme_btn.style.setProperty('filter', 'none');
+
+		for (var i = 0; i < plinks.length; i++) {
+			plinks[i].style.setProperty('filter', 'none');
+		}
+		for (var i = 0; i < footer_icons.length; i++) {
+			footer_icons[i].style.setProperty('filter', 'none');
+		}
+
+		localStorage.setItem('theme', 'dark');
+	}
+}
+
+function init() {
+	roleElement.innerText = roles[roleIndex];
+	setTimeout(() => {
+		removeInterval = setInterval(removeText, 200);
+	}, 500);
+
+	var imgs = document.getElementsByTagName('img');
+	imgs = [...imgs];
+	imgs.forEach((img) => {
+		img.ondragstart = () => {
+			return false;
+		};
+	});
+
+	if (localStorage.getItem('theme') == 'light') {
+		switchTheme();
+	}
+
+	const themeBtn = document.getElementById('themeBtn');
+	themeBtn.addEventListener('click', switchTheme);
+
+	// manually set skill images?
 }
 
 init();
-
